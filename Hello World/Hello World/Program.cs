94a7685cf4c660757089f4a1b8e014994a766fe6
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-07-31 03:00:08 -0500

    ALS

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Original instructions
+            Console.WriteLine("Hello, World!");
+
+            // Challenge
+            Console.WriteLine("Goodbye, World!");
+        }
+    }
+}
