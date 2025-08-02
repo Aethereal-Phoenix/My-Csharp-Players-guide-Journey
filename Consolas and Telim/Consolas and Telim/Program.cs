b8e8b7efacc8cc9a8db5aa996ed7c2ade274b0b7
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-02 02:20:37 -0500

    ALS

---
+++ Consolas and Telim/Consolas and Telim/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Security;
+
+namespace Consolas_and_Telim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bread for the city is complete.");
+            Console.WriteLine("Please enter the name of the recipient:");
+            string recipientName = Console.ReadLine();
+            Console.WriteLine($"Recipient: {recipientName}");
+        }
+    }
+}
