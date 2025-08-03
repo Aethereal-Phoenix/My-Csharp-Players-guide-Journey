2829eb9a04b3139698a84ab8bb5aa400b9056124
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-03 02:43:44 -0500

    ALS

---
+++ The Thing Namer 3000/The Thing Namer 3000/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace The_Thing_Namer_3000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What kind of thing are we talking about?");
+            string a = Console.ReadLine(); // Variable should be called thing
+            Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+            string b = Console.ReadLine(); // Variable should be called thingDescription
+            string c = " of Doom"; /*I do not understand why this is a variable*/ // Added a space to make itwork better. 
+            string d = "3000"; // Again this doesnt need to be a variable
+            Console.WriteLine("The " + b + " " + a /*There was a " of " here but it is not needed because c already has it*/+ c + " " + d + "!");
+        }
+    }
+}
