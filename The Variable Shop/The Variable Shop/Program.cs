a989e5f8dfdb724ae9dfd3a02351ddfb71ec9a91
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-04 09:44:26 -0500

    The Variable Shop

---
+++ The Variable Shop/The Variable Shop/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace The_Variable_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringVariable = "This is a string variable.";
+            char charvariable = 'A';
+            bool boolVariable = false;
+            byte byteVariable = 255;
+            short shortVariable = 32_767;
+            int intVariable = 2_147_483_647;
+            long longVariable = 9_223_372_036_854_775_807;
+            sbyte sbyteVariable = 127;
+            ushort ushortVariable = 65_535;
+            uint uintVariable = 4_294_967_295;
+            ulong ulongVariable = 18_446_744_073_709_551_615;
+            float floatVariable = 3.14159f;
+            double doubleVariable = 456.123;
+            decimal decimalVariable = 25.86m;
+
+
+            Console.WriteLine(stringVariable);
+            Console.WriteLine(charvariable);
+            Console.WriteLine(boolVariable);
+            Console.WriteLine(byteVariable);
+            Console.WriteLine(shortVariable);
+            Console.WriteLine(intVariable);
+            Console.WriteLine(longVariable);
+            Console.WriteLine(sbyteVariable);
+            Console.WriteLine(ushortVariable);
+            Console.WriteLine(uintVariable);
+            Console.WriteLine(ulongVariable);
+            Console.WriteLine(floatVariable);
+            Console.WriteLine(doubleVariable);
+            Console.WriteLine(decimalVariable);
+
+
+        }
+    }
+}
