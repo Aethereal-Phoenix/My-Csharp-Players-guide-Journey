cacfb6f2991f8de2737e76404a714a8557c5b69d
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-05 10:32:10 -0500

    The Four Sisters and the Duckbear

---
+++ The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace The_Four_Sisters_and_the_Duckbear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of eggs that were collected today:");
+            int eggsCollected = int.Parse(Console.ReadLine());
+
+            int eggsForDuckbear = eggsCollected % 4;
+            int eggsForEachSister = (eggsCollected - eggsForDuckbear) / 4;
+
+            Console.WriteLine($"Each sister will get: {eggsForEachSister}");
+            Console.WriteLine($"The Duckbear will get: {eggsForDuckbear}");
+        }
+    }
+}
