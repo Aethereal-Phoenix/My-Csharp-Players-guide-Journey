716729a5fcbb80f95e5201e2c72fb591c188b4ea
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-05 07:07:48 -0500

    The Triangle Farmer

---
+++ The Triangle Farmer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace The_Triangle_Farmer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the size of the base of the triangle:");
+            double triangleBase = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the height of the triangle:");
+            double triangleHeight = double.Parse(Console.ReadLine());
+
+            double triangleArea = ((triangleBase * triangleHeight) / 2);
+
+            Console.WriteLine($"Area: {triangleArea}");
+        }
+    }
+}
