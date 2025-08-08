e70413bb291025f074378d60ce796159f529adee
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-08 11:45:16 -0500

    The Magic Canon

---
+++ The Magic Cannon/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace The_Magic_Cannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (!(0 == i % 3) && !(0 == i % 5))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"{i} -- Normal");
+                }
+                else if (0 == i % 3 && 0 == i % 5)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{i} -- Fire & Lightning");
+
+                }
+                else if (0 == i % 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{i} -- Fire");
+                }
+                else if (0 == i % 5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{i} -- Lightning");
+                }
+            }
+        }
+    }
+}
