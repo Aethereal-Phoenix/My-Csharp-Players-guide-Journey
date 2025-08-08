587599e624b06694877b91e1b36e363f2b7cc5fe
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-08 02:31:33 -0500

    Watchtower

---
+++ Watchtower/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Watchtower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Watchtower";
+
+            Console.Write("Please enter the x axis value for the enemies position: ");
+            int xValue = int.Parse(Console.ReadLine());
+            Console.Write("Please enter the y axis value fo the enemies position: ");
+            int yValue = int.Parse(Console.ReadLine());
+
+            if (0 > xValue)
+            {
+                if (0 < yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the NW");
+                }
+                else if (0 == yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the W");
+                }
+                else if (0 > yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the SW");
+                }
+            }
+            else if ( 0 == xValue)
+            {
+                if (0 < yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the North");
+                }
+                else if (0 == yValue)
+                {
+                    Console.WriteLine("The enemy is already within our walls");
+                }
+                else if (0 > yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the S");
+                }
+            }
+            else if ( 0 < xValue)
+            {
+                if (0 < yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the NE");
+                }
+                else if (0 == yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the E");
+                }
+                else if (0 > yValue)
+                {
+                    Console.WriteLine("The enemy approaches from the SE");
+                }
+            }
+        }
+    }
+}
