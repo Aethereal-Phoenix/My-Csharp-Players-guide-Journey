1c275701e39e2ac8df010dd9cb1fbaa882d88083
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-09 08:08:46 -0500

    Countdown

---
+++ Countdown/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to countdown from");
+            int userInput = int.Parse(Console.ReadLine());
+            Countdown(userInput);
+            
+            void Countdown(int currentNumber)
+            {
+                if (currentNumber == 0)
+                {
+                    return;
+                }
+                Console.WriteLine(currentNumber);
+                Countdown(currentNumber - 1);
+            }
+        }
+    }// Methods go above this line
+}
