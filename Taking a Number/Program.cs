2bbbff9f3ae9c1327ce727e31c8ea3613ffed989
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-09 07:34:18 -0500

    Taking a Number

---
+++ Taking a Number/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Taking_a_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+        int AskForNumberInRange(string text,  int min, int max)
+        {
+            while (true)
+            {
+                Console.WriteLine(text);
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (min > userInput || max < userInput)
+                {
+                    Console.WriteLine($"{userInput} is out of the approved parameters. Please try again.");
+                }
+                else
+                {
+                    return userInput;
+                }
+            }
+        }
+    }// Methods go above this line
+}
