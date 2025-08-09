a59a8c7070e41f528d6d879cac5dd71771783f9c
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-09 02:56:56 -0500

    The Laws of Freach

---
+++ The laws of Freach/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace The_laws_of_Freach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            int currentSmallest = int.MaxValue; //Start higher than anything in the array.
+            
+            foreach (int index in array)
+            {
+                if(index < currentSmallest)currentSmallest = index;
+            }
+            
+            Console.WriteLine(currentSmallest);
+        }
+    }
+}
