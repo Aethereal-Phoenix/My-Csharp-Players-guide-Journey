e1b532ab52b8f5ed9777f7414987b672596671df
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-12 12:45:56 -0500

    ALS

---
+++ Tester/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter an int i.e 123");
+                try
+                {
+                    int userInt = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("You entered an invalid format. Please try again.");
+                    Console.WriteLine(ex);
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("This is for all exceptions, not just a specific type.");
+                    Console.WriteLine(ex);
+                    continue;
+                }
+                Console.WriteLine("You have entered a valid input");
+                break;
+            }
+            Console.WriteLine("You have exited the while loop.");
+        }
+    }
+}
