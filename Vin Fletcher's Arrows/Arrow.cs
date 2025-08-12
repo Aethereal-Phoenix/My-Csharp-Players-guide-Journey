f6c1f887e0d04cfc2e36ad928d6f6fe8397e8d14
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-12 13:53:13 -0500

    Vin Fletcher's Arrows

---
+++ Vin Fletcher's Arrows/Arrow.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vin_Fletcher_s_Arrows
+{
+    enum Arrowhead
+    {
+        None,
+        Steel,
+        Wood,
+        Obsidian
+    }
+
+    enum Fletching
+    {
+        None,
+        Plastic,
+        Turkey,
+        Goose
+    }
+    internal class Arrow
+    {
+        // Properties
+        public Arrowhead _arrowhead;
+        public Fletching _fletching;
+        public int _length_cm;
+
+        // Methods
+        public Arrowhead ArrowheadSelector()
+        {
+            while (true)
+            {
+                Console.WriteLine("What sort of arrowhead would you like?");
+                foreach (Arrowhead arrowhead in Enum.GetValues(typeof(Arrowhead)))
+                {
+                    Console.WriteLine($"{arrowhead}");
+                }
+                Console.WriteLine("Please enter the name of the arrowhead you would like?");
+                string userChoice = Console.ReadLine();
+
+                foreach (Arrowhead arrowhead in Enum.GetValues(typeof(Arrowhead)))
+                {
+                    string arrowheadString = arrowhead.ToString();
+                    if (userChoice.ToUpper() == arrowheadString.ToUpper())
+                    {
+                        return arrowhead;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+        }
+
+        public Fletching FletchingSelector()
+        {
+            while (true)
+            {
+                Console.WriteLine("What sort of arrowhead would you like?");
+                foreach (Fletching fletching in Enum.GetValues(typeof(Fletching)))
+                {
+                    Console.WriteLine($"{fletching}");
+                }
+                Console.WriteLine("Please enter the name of the arrowhead you would like?");
+                string userChoice = Console.ReadLine();
+
+                foreach (Fletching fletching in Enum.GetValues(typeof(Fletching)))
+                {
+                    string fletchingString = fletching.ToString();
+                    if (userChoice.ToUpper() == fletchingString.ToUpper())
+                    {
+                        return fletching;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+        }
+
+        public int LengthSelector()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter the length of the arrow shaft in increments of 1 cm.\nThe arrow can be as short as 60 cm and as long as 100 cm");
+                int length = int.Parse(Console.ReadLine());
+
+                if (length < 60 || length > 100)
+                {
+                    Console.WriteLine("That length is not available please try again.");
+                    continue;
+                }
+                else
+                {
+                    while (true)
+                    {
+                        Console.WriteLine($"You have selected: {length}\nIs this correct? Y/N");
+                        string userChoice = Console.ReadLine();
+
+                        if (userChoice.ToUpper() != "Y" && userChoice.ToUpper() != "N")
+                        {
+                            Console.WriteLine("Please select either Y or N");
+                        }
+                        else if (userChoice.ToUpper() == "Y")
+                        {
+                            return length;
+                        }
+                        else if (userChoice.ToUpper() == "N")
+                        {
+                            Console.WriteLine("Very well. We will try again.");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        // Constructor
+        public Arrow()
+        {
+            _arrowhead = ArrowheadSelector();
+            _fletching = FletchingSelector();
+            _length_cm = LengthSelector();
+        }
+    }// End of Arrow Class
+}
---
+++ Vin Fletcher's Arrows/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Vin_Fletcher_s_Arrows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Lets make and arrow");
+                Arrow arrow = new Arrow();
+                double arrowPrice = ArrowPrice(arrow);
+                Console.WriteLine($"The total cost of that arrow is: {arrowPrice}");
+                while (true)
+                {
+                    Console.WriteLine("Is this correct? Y/N");
+                    string userInput = Console.ReadLine();
+                    
+                    if (userInput.ToUpper() != "Y" && userInput.ToUpper() != "N")
+                    {
+                        Console.WriteLine("Invalid input please try again.");
+                        continue;
+                    }
+                    else if (userInput.ToUpper() == "Y")
+                    {
+                        break;
+                    }
+                    else if (userInput.ToUpper() == "N")
+                    {
+                        return;
+                    }
+                }
+                Console.WriteLine("How many would you like to order?");
+                double totalArrows = int.Parse(Console.ReadLine());
+                
+                while (true)
+                {
+                    Console.WriteLine($"{totalArrows} Is this correct? Y/N");
+                    string userInput = Console.ReadLine();
+
+                    if (userInput.ToUpper() != "Y" && userInput.ToUpper() != "N")
+                    {
+                        Console.WriteLine("Invalid input please try again.");
+                        continue;
+                    }
+                    else if (userInput.ToUpper() == "Y")
+                    {
+                        break;
+                    }
+                    else if (userInput.ToUpper() == "N")
+                    {
+                        return;
+                    }
+                }
+                double totalCost = totalArrows * arrowPrice;
+                Console.WriteLine($"Grand total: {totalCost}");
+            }
+        }
+        public static double ArrowPrice(Arrow arrow)
+        {
+            double arrowheadCost = 0;
+            double fletchingCost = 0;
+            double lengthCost = 0;
+            double totalCost = 0;
+
+            if (arrow._arrowhead == Arrowhead.Steel)
+            {
+                arrowheadCost = 10.00;
+            }
+            else if (arrow._arrowhead == Arrowhead.Wood)
+            {
+                arrowheadCost = 3.00;
+            }
+            else if (arrow._arrowhead == Arrowhead.Obsidian)
+            {
+                arrowheadCost = 5.00;
+            }
+
+            if (arrow._fletching == Fletching.Plastic)
+            {
+                fletchingCost = 10.00;
+            }
+            else if (arrow._fletching == Fletching.Turkey)
+            {
+                fletchingCost = 5.00;
+            }
+            else if (arrow._fletching == Fletching.Goose)
+            {
+                fletchingCost = 3.00;
+            }
+
+            lengthCost = arrow._length_cm * .05;
+
+            totalCost = arrowheadCost + fletchingCost + lengthCost;
+            return totalCost;
+        }
+    }// Methods go above this line
+}
