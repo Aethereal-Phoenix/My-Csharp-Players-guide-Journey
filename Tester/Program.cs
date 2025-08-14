bf091942a49ac240444e3fe2dfc731af4df47dae
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-08-14 02:42:50 -0500

    Vin's Trouble

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -4,29 +4,52 @@
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine("Please enter an int i.e 123");
-                try
-                {
-                    int userInt = int.Parse(Console.ReadLine());
-                }
-                catch (FormatException ex)
-                {
-                    Console.WriteLine("You entered an invalid format. Please try again.");
-                    Console.WriteLine(ex);
-                    continue;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("This is for all exceptions, not just a specific type.");
-                    Console.WriteLine(ex);
-                    continue;
-                }
-                Console.WriteLine("You have entered a valid input");
-                break;
-            }
-            Console.WriteLine("You have exited the while loop.");
+            //while (true)
+            //{
+            //        Console.WriteLine("Please enter an int i.e 123");
+            //        try
+            //        {
+            //            int userInt = int.Parse(Console.ReadLine());
+            //        }
+            //        catch (FormatException ex)
+            //        {
+            //            Console.WriteLine("You entered an invalid format. Please try again.");
+            //            Console.WriteLine(ex);
+            //            continue;
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            Console.WriteLine("This is for all exceptions, not just a specific type.");
+            //            Console.WriteLine(ex);
+            //            continue;
+            //        }
+            //        Console.WriteLine("You have entered a valid input");
+            //        break;
+            //    }
+            //    Console.WriteLine("You have exited the while loop.");
+            //}
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Please enter a number between 1-10");
+            //    string userInput = Console.ReadLine();
+
+            //    string output = userInput switch
+            //    {
+            //        "1" => "One",
+            //        "2" => "Two",
+            //        "3" => "Three",
+            //        "4" => "Four",
+            //        "5" => "Five",
+            //        "6" => "Six",
+            //        "7" => "Seven",
+            //        "8" => "Eight",
+            //        "9" => "Nine",
+            //        "10" => "Ten"
+            //    };
+            //    Console.WriteLine(output);
+            //    Console.ReadKey(true);
+            //}
         }
     }
 }
